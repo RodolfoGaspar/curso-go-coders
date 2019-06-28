83b7c721ab20d81294b2dca1aa80495f84e2598e
Rodolfo Gaspar <dolfogaspar@gmail.com>
2019-06-27 23:20:16 -0300

    aula 19 - operadores logicos

--- fundamentos/logicos/logicos.go
+++ fundamentos/logicos/logicos.go
@@ -1,3 +1,29 @@
 package main
 
-func compras()
+import "fmt"
+
+func compras(trab1, trab2 bool) (bool, bool, bool) {
+	comprarTv50 := trab1 && trab2
+	comprarTv32 := trab1 != trab2 // ou exclusivo
+	tomarSorvete := trab1 || trab2
+
+	return comprarTv32, comprarTv50, tomarSorvete
+}
+
+func main() {
+	tv50, tv32, sorvete := compras(true, true)
+	fmt.Printf("\nTv50: %t, Tv32: %t, Sorvete: %t, Saudável: %t",
+		tv50, tv32, sorvete, !sorvete)
+
+	tv50, tv32, sorvete = compras(false, true)
+	fmt.Printf("\nTv50: %t, Tv32: %t, Sorvete: %t, Saudável: %t",
+		tv50, tv32, sorvete, !sorvete)
+
+	tv50, tv32, sorvete = compras(true, false)
+	fmt.Printf("\nTv50: %t, Tv32: %t, Sorvete: %t, Saudável: %t",
+		tv50, tv32, sorvete, !sorvete)
+
+	tv50, tv32, sorvete = compras(false, false)
+	fmt.Printf("\nTv50: %t, Tv32: %t, Sorvete: %t, Saudável: %t",
+		tv50, tv32, sorvete, !sorvete)
+}
