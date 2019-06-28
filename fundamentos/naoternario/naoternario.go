e20d86e2de69d22dc08f01614780b00733840bbb
Rodolfo Gaspar <dolfogaspar@gmail.com>
2019-06-27 23:40:36 -0300

    aula 21 - operador ternario (nao tem)

---
+++ fundamentos/naoternario/naoternario.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// golang não tem operador ternario
+func obterResultado(nota float64) string {
+	if nota >= 6 {
+		return "Aprovado"
+	}
+	return "Reprovado"
+
+	// return nota>=6 ? "Aprovado" : "Reprovado"
+}
+
+func main() {
+	fmt.Println(obterResultado(6.3))
+}
