eab33fa464134f47d228d41496e0a645dbce17e6
Rodolfo Gaspar <dolfogaspar@gmail.com>
2019-07-23 21:43:22 -0300

    aula 44 - passando função como parametro

---
+++ funcoes/comoparametro/comoparametro.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// map, reduce e filter são bons exemplos de utilização de função como parametro
+
+func main() {
+
+	fmt.Println(exec(multiplicacao, 2, 3))
+}
+
+func multiplicacao(a, b int) int {
+	return a * b
+}
+
+func exec(funcao func(int, int) int, p1, p2 int) int {
+	return funcao(p1, p2)
+}
