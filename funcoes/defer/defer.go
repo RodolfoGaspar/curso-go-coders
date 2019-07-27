6d6c345a67ab65688798a65e40af0fb6f507c0ba
Rodolfo Gaspar <dolfogaspar@gmail.com>
2019-07-26 21:21:33 -0300

    aula 50 - defer

--- funcoes/defer/defer.go
+++ funcoes/defer/defer.go
@@ -1,18 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	fmt.Println(obterValorAprovado(6000))
+	fmt.Println(obterValorAprovado(600))
 }
 
 func obterValorAprovado(numero int) int {
 	defer fmt.Println("Fim!")
 
-	if numero>=5000 {
+	if numero > 5000 {
 		fmt.Println("Valor Alto")
 		return 5000
 	}
-	else {
-		fmt.Print("Valor Baixo")
-		return numero
-	}
+	fmt.Println("Valor Baixo")
+	return numero
 }
